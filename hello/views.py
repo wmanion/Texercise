e784f1953562d07b35fcdccecf987ff28a10a9db
Wayne Manion <wmanion@unomaha.edu>
2015-02-08 13:06:20 -0600

    Demo

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -6,9 +7,8 @@ from django.http import HttpResponse
 from .models import Greeting
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 def db(request):
 
