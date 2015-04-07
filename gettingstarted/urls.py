564b2fec7c605f445fc5b6ba6f281db58459eb84
Wayne Manion <wmanion@unomaha.edu>
2015-04-06 22:44:48 -0500

    fixing problems

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
 
